f8a7d797ce7c6ccf7434cc9629118dbe9ac7f5e7
Bowgo Tsai <bowgotsai@google.com>
2020-03-03 12:15:50 +0800

    haydn: Removing GSI keys
    Those keys will be embedded into VtsSecurityAvb on host side
    instead, to verify the GSI image used on the device.
    
    Bug: 149806769
    Test: build and checks those keys are removed from
          $OUT/recovery/root/first_stage_ramdisk/avb/
    Change-Id: I8a002ba6f1421fb460056ccae6572050bdb0ce3c

--- device.mk
+++ device.mk
@@ -7,9 +7,6 @@
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
