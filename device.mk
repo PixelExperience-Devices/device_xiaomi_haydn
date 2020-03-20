847891659aa219112dcf334c6c02a61967a996fa
Arian <arian.kulmer@web.de>
2020-03-20 23:42:25 +0100

    haydn: parts: Import from xiaomi_sdm845-common
    https://github.com/LineageOS/android_device_xiaomi_sdm845-common/tree/e261035d91b34cf0fdf0f012927e06a2bcdb03ed/parts
    
    Change-Id: I442f48780ca020f3e420ee4ccaf8ac07b05457cb

--- device.mk
+++ device.mk
@@ -186,6 +186,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
