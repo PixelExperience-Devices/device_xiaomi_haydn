cd1aece7652559e0ed969576e7fe64087d73531c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:01:44 +0100

    haydn: Build health HIDL
    Change-Id: I89c431c7ac87fe878537375cd811606d68f4bb1d

--- device.mk
+++ device.mk
@@ -178,6 +178,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-service
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
