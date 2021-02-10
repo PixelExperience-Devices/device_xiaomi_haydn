3955978d2388bab7caf617460e5d57155611ef88
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:09:03 +0100

    haydn: Build QTI USB HIDL
    Change-Id: Id7da15ac872967e4fb733836c99efaf39aca3a56

--- device.mk
+++ device.mk
@@ -324,3 +324,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.2-service-qti
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
