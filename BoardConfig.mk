5bfb6eee06de5cacab5354eacf5a75657ad09351
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-07 10:48:40 +0300

    haydn: add vendor prop list

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -218,6 +218,7 @@ TARGET_BOARD_PLATFORM := lahaina
 
 # Properties
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
