76ff0a1b4598fdbd6868d30ae2dfa3bfc2dbc3ab
EcrosoftXiao <xy19990903@gmail.com>
2021-10-09 10:11:17 +0800

    haydn: Inherit missing basic stuffs

--- aosp_haydn.mk
+++ aosp_haydn.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from haydn device
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
