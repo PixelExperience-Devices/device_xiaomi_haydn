c190a514a9c5e8264dba47aab162c6910bdec7c8
EcrosoftXiao <xy19990903@gmail.com>
2021-10-10 09:16:22 +0800

    haydn: Build perf hal

--- device.mk
+++ device.mk
@@ -251,6 +251,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
+    vendor.qti.hardware.perf@2.2.vendor
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
