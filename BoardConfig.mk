db5f1d80ec5ee7dfc5ef5920a0ae91de59e2aeda
EcrosoftXiao <xy19990903@gmail.com>
2021-10-10 09:16:22 +0800

    haydn: Import basic sepolicy stuffs from sm8350-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -250,6 +250,10 @@ VENDOR_SECURITY_PATCH := 2021-07-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Treble
 BOARD_VNDK_VERSION := current
 
