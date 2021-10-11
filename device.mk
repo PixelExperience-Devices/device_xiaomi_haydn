da692917c4856c4e49b9eeca0e69e745f6ed1e9d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 01:00:17 +0200

    haydn: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?
    
    Change-Id: I0f1713ee542102374183ef8088bcad62d3859c2f

--- device.mk
+++ device.mk
@@ -135,7 +135,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
