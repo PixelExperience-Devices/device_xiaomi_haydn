d256542ab658c4ecc736b0a25b442f777ab77907
EcrosoftXiao <xy19990903@gmail.com>
2021-11-09 17:31:12 +0800

    fixup! haydn: Build ANT+ libs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,9 @@ BUILD_BROKEN_DUP_RULES := true
 # APEX
 DEXPREOPT_GENERATE_APEX_IMAGE := true
 
+# ANT+
+BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
