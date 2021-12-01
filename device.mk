affa6d16e5cca5d6b1f26a8ec378dfdb72093122
EcrosoftXiao <xy19990903@gmail.com>
2021-12-02 00:04:46 +0800

    Revert "haydn: Move to AOSP NXP NFC HAL"
    This reverts commit a171358518c2b3ce16180fd132beb610933d528d.

--- device.mk
+++ device.mk
@@ -345,14 +345,27 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
 
 # NFC
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/nxp/opensource/sn100x
+
 PRODUCT_PACKAGES += \
-    android.hardware.nfc_snxxx@1.2-service \
-    android.hardware.secure_element_snxxx@1.2-service \
     com.android.nfc_extras \
     libchrome.vendor \
-    NfcNci \
+    com.nxp.nfc.nq \
+    NQNfcNci \
     SecureElement \
     Tag
+
+PRODUCT_PACKAGES += \
+    android.hardware.secure_element@1.2.vendor \
+    libnqnfc_nci_jni \
+    nfc_nci.nqx.default.hw \
+    vendor.nxp.hardware.nfc@2.0-service
+
+PRODUCT_PACKAGES += \
+    jcos_nq_client \
+    ls_nq_client \
+    se_nq_extn_client
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
