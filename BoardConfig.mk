EcrosoftXiao/device_xiaomi_cepheus

haydn: use SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
BOARD_PLAT_PRIVATE_SEPOLICY_DIR and BOARD_PLAT_PUBLIC_SEPOLICY_DIR
has been deprecated.

Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

BoardConfig.mk
@@ -184,8 +184,8 @@ VENDOR_SECURITY_PATCH := 2021-07-01
 TARGET_SEPOLICY_DIR := msmnile
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # USB

PixelExperience-Devices/device_xiaomi_haydn

BoardConfig.mk
@@ -258,8 +258,8 @@ VENDOR_SECURITY_PATCH := 2021-07-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble