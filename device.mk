8022f89c742f6b791f7e5d9743347ba065968b41
AdarshGrewal <adarshgrewal@gmail.com>
2022-02-10 20:50:54 +0530

    haydn: Add recovery init

--- device.mk
+++ device.mk
@@ -112,6 +112,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Recovery
+PRODUCT_PACKAGES += \
+    init.recovery.qcom.rc
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
