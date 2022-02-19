96ab8380527619fb7fd8cd5124079b89d9ebf460
AdarshGrewal <adarshgrewal@gmail.com>
2022-02-20 00:27:27 +0530

    haydn: Build 1.3 DRM clearkey service

--- device.mk
+++ device.mk
@@ -163,6 +163,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.3-service.clearkey
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
