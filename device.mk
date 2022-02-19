f10dbec066113d4986342920667535c6621b47fe
AdarshGrewal <adarshgrewal@gmail.com>
2022-02-19 23:52:57 +0530

    haydn: Build QTI camera postproc

--- device.mk
+++ device.mk
@@ -132,6 +132,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
