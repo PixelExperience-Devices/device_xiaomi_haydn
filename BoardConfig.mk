b50f7bd2bb95fcae20ab8936699857bd59d9828b
AdarshGrewal <adarshgrewal@gmail.com>
2022-02-19 22:57:21 +0530

    haydn: Include common lineage FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 
