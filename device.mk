0af0e58a26733ca286175f00385f9ef0e57f36de
AdarshGrewal <adarshgrewal@gmail.com>
2022-02-20 13:41:32 +0530

    haydn: Add HotwordEnrollement privapp permissions

--- device.mk
+++ device.mk
@@ -202,6 +202,10 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder.vendor
 
+# HotwordEnrollement
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.class_main.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.class_main.sh \
