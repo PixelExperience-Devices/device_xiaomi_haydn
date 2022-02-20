fba8704b6cb228e9017ee423f9b63d2e107d1ffc
AdarshGrewal <adarshgrewal@gmail.com>
2022-02-20 12:25:54 +0530

    haydn: Build vndservicemanager

--- device.mk
+++ device.mk
@@ -317,6 +317,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# VNDK
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
