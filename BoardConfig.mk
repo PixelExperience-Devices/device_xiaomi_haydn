694a51071d66c175872f6192fa1aea435fe9be7d
Erfan Abdi <erfangplus@gmail.com>
2022-03-04 22:01:12 +0530

    haydn: sepolicy: Add mlipay rules
    Change-Id: I1023e481d49ed5ec0ae15a513c65133987d6f556

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,6 +188,7 @@ VENDOR_SECURITY_PATCH := 2021-12-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
