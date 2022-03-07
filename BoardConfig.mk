2333faf221147a13828063f401a8af9d5504a5ee
Arian <arian.kulmer@web.de>
2022-03-07 14:00:40 +0100

    haydn: Build GPS HAL from source and import proprietary configs
    Configs from haydn V13.0.1.0.SKKMIXM
    slim_daemon is disabled in izat.conf, hence drop it.
    
    Change-Id: Ia1ed33f6250b898d149970389befe8033cc8eaf3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,6 +80,10 @@ TARGET_USES_HWC2 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+LOC_HIDL_VERSION := 4.0
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/aosp/config/device_framework_matrix.xml
