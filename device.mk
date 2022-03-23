0c1c9a8f636fa014688387151e1b730868b9c45d
AdarshGrewal <adarshgrewal@gmail.com>
2022-03-23 22:02:39 +0530

    haydn: Update from haydn V13.0.1.0 SKKMIXM 12.0

--- device.mk
+++ device.mk
@@ -110,6 +110,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
