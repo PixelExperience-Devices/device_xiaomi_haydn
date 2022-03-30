576b640070eb93f11527aa62a96713addef904bc
sadoyui <102640681+sadoyui@users.noreply.github.com>
2022-03-30 11:22:10 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,6 +18,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_haydn.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_haydn-user \
-    aosp_haydn-userdebug \
-    aosp_haydn-eng
+    lineage_haydn-user \
+    lineage_haydn-userdebug \
+    lineage_haydn-eng
