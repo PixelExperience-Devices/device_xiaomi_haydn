d2b5fbe20dc3caddf5da4474d7e928e40e18e923
AdarshGrewal <adarshgrewal@gmail.com>
2022-04-02 14:31:23 +0530

    haydn: Switch to AOSP WFD

--- extract-files.sh
+++ extract-files.sh
@@ -58,9 +58,6 @@ function blob_fixup() {
         vendor/etc/camera/pureShot_parameter.xml)
             sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
             ;;
-        system_ext/lib64/libwfdnative.so)
-            patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
-            ;;
         vendor/lib64/hw/camera.xiaomi.so)
             hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/52070094/1F2003D5/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
             mv "${TMPDIR}/${1##*/}" "${2}"
