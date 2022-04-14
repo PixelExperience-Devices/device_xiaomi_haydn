882295596287c4ee8e85560fc6ecd3fd1040e4d8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-15 00:14:19 +0200

    IFAAService: Initial implementation
    Co-authored-by: Art_Chen <chenxy0201@qq.com>
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    
    Change-Id: Ia238f3e82e6366e72de4629b31864d0503dd1a03

--- device.mk
+++ device.mk
@@ -241,6 +241,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    IFAAService
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
