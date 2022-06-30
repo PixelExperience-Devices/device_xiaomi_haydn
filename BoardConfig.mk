dc3456968859ea610811d624e39014d16d905487
AdarshGrewal <adarshgrewal@gmail.com>
2022-06-30 23:51:47 +0530

    haydn: sepolicy: Address mlipay denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,6 +196,7 @@ VENDOR_SECURITY_PATCH := 2022-05-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
